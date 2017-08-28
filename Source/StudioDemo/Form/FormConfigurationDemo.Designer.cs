7e923a16221aee1eea30fb0d59c36fd085b72b18
lincenses <lincenses@outlook.com>
2017-08-28 17:58:51 +0800

    lin

--- Source/StudioDemo/Form/FormConfigurationDemo.Designer.cs
+++ Source/StudioDemo/Form/FormConfigurationDemo.Designer.cs
@@ -28,20 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormConfigurationDemo));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(784, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(120, 22);
+            this.toolStripButton1.Text = "创建数据库配置文件";
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(120, 22);
+            this.toolStripButton2.Text = "读取数据库配置文件";
+            // 
+            // treeView1
+            // 
+            this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.treeView1.Location = new System.Drawing.Point(12, 28);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(760, 421);
+            this.treeView1.TabIndex = 1;
             // 
             // FormConfigurationDemo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.toolStrip1);
             this.Name = "FormConfigurationDemo";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "FormConfigurationDemo";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.TreeView treeView1;
     }
 }
--- Source/StudioDemo/Form/FormConfigurationDemo.cs
+++ Source/StudioDemo/Form/FormConfigurationDemo.cs
@@ -14,6 +14,46 @@ namespace StudioDemo
         public FormConfigurationDemo()
         {
             InitializeComponent();
+            InitializeEvent();
         }
+
+        #region 初始化事件
+        private void InitializeEvent()
+        {
+            toolStripButton1.Click += ToolStripButton1_Click;
+            toolStripButton2.Click += ToolStripButton2_Click;
+        }
+
+        
+        #endregion
+
+        #region 创建数据库配置文件
+        private void ToolStripButton1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.InitialDirectory = Application.StartupPath + "\\Configuration";
+            if (!System.IO.Directory.Exists(dialog.InitialDirectory))
+            { System.IO.Directory.CreateDirectory(dialog.InitialDirectory); }
+            dialog.FileName = "Database.cfg";
+            dialog.Filter = "*.cfg|*.cfg";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                Studio.Configuration.DatabaseConfiguration configuration = new Studio.Configuration.DatabaseConfiguration();
+                configuration.DataSource = ".\\SQL2012";
+                configuration.InitialCatalog = "Sems";
+                configuration.UserID = "sa";
+                configuration.Password = "Admin123456";
+                configuration.Save(dialog.FileName, true);
+                
+            }
+        }
+        #endregion
+
+        #region 读取数据库配置文件
+        private void ToolStripButton2_Click(object sender, EventArgs e)
+        {
+            
+        }
+        #endregion
     }
 }
