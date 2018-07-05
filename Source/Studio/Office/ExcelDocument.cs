48549e3986ac7805ee694e522bb2ca81f102c5b1
lincenses <lincenses@outlook.com>
2018-07-05 10:57:47 +0800

    lin

--- Source/Studio/Office/ExcelDocument.cs
+++ Source/Studio/Office/ExcelDocument.cs
@@ -10,6 +10,25 @@ namespace Studio.Office
         [System.Runtime.InteropServices.DllImport("user32.dll", CharSet = System.Runtime.InteropServices.CharSet.Auto)]
         private static extern int GetWindowThreadProcessId(IntPtr hwnd, out int ID);
 
+        #region 私有成员
+
+        /// <summary>
+        /// Excel对象。
+        /// </summary>
+        private Microsoft.Office.Interop.Excel.Application _Excel;
+
+        /// <summary>
+        /// 是否已被释放。
+        /// </summary>
+        private bool _IsDisposed = false;
+
+        /// <summary>
+        /// 进程ID。
+        /// </summary>
+        private int _ProcessID = 0;
+
+        #endregion
+
 
     }
 }
