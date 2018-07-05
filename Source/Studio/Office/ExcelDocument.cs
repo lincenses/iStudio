b5db78f0a00aacd4944c3e78a300bb1c8b45515b
lincenses <lincenses@outlook.com>
2018-07-05 10:45:56 +0800

    lin

---
+++ Source/Studio/Office/ExcelDocument.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Studio.Office
+{
+    public class ExcelDocument
+    {
+        [System.Runtime.InteropServices.DllImport("user32.dll", CharSet = System.Runtime.InteropServices.CharSet.Auto)]
+        private static extern int GetWindowThreadProcessId(IntPtr hwnd, out int ID);
+
+
+    }
+}
